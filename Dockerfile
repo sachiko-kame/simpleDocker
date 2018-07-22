dd39c5bc1f3be12b0826e8dc9007761b254c11fe
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-07-22 12:15:14 +0900

    js追加

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM nginx
 COPY ./html /usr/share/nginx/html
+RUN apt-get update && apt-get install -y nodejs
---
+++ html/js/simple.js
@@ -0,0 +1,11 @@
+function confirmFunction1() {
+    if(window.confirm('移動しますか？')){
+        location.href = "./link/Nutrition.html";
+    }else{
+        window.alert('キャンセルされました');
+    }
+}
+
+function clickEvent() {
+    alert("hello, world.");
+}
